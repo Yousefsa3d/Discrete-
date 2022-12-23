c0bcac48ca715d71647642edb225ef2e5527c3c2
Yousefsa3d <121289533+Yousefsa3d@users.noreply.github.com>
2022-12-23 18:07:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace prime_numbers2
+{
+    class Program
+    {
+        public Program()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+
+            int num, i, flag, n1, n2;
+            Console.WriteLine("Enter first number ");
+
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number ");
+
+            n2 = int.Parse(Console.ReadLine());
+            num = n1;
+            Console.WriteLine("      \n  Prime Numbers                   ");
+
+            while (num <= n2)
+            {
+                flag = 0;
+                for (i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+
+                    Console.WriteLine(num);
+                num++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
